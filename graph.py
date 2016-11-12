fc15b056e8c11f889eec5901220b5cc5c65268cc
Bruno Marques <brunomn95@gmail.com>
2016-11-12 17:26:18 -0200

    Fixing identantion

--- graph.py
+++ graph.py
@@ -16,19 +16,19 @@ class Graph(object):
         """ Remove o vértice "vertex" do grafo, e as conexões que o envolvam """
         if vertex in self.dict:
             del self.dict[vertex]
-            for vertexes in self.dict
-                if vertex in self.dict[vertexes]
+            for vertexes in self.dict:
+                if vertex in self.dict[vertexes]:
                     self.dict[vertexes].remove(vertex)
 
     def connect(self, v1, v2):
         """ Conecta dois vértices do grafo """
-        if v1 in self.dict and v2 in self.dict
+        if v1 in self.dict and v2 in self.dict:
             self.dict[v1].add(v2)
             self.dict[v2].add(v1)
 
     def disconnect(self, v1, v2):
         """ Desconecta dois vértices do grafo """
-        if v1 in self.dict and v2 in self.dict
+        if v1 in self.dict and v2 in self.dict:
             self.dict[v1].discard(v2)
             self.dict[v2].discard(v1)
 
@@ -46,16 +46,57 @@ class Graph(object):
 
     def get_adjacents(self, vertex):
         """ Informa os vértices adjacentes ao vértice "vertex" """
-        if vertex in self.dict
+        if vertex in self.dict:
             return self.dict[vertex]
           
     def get_degree(self, vertex):
         """ Informa o grau do vértice "vertex" """
-        if vertex in self.dict
+        if vertex in self.dict:
             return len(self.dict[vertex])
 
     def is_regular(self):
+        """ Verifica se todos vértices do grafo possuem mesmo grau """
+        base_degree = self.get_degree(list(self.dict.keys())[0])
+        return not any(self.get_degree(vertex) != base_degree
+                       for vertex in self.dict)
+
     def is_complete(self):
-    def get_transitive_closure(self, vertex):
+        """ Verifica se o grafo é completo """
+        for vertexes in self.dict:
+            if (self.dict[vertexes].union([vertex])) != set(self.dict.keys()):
+                return False
+        return True
+
+    def get_transitive_closure(self, vertex, transitive_clousure=set()):
+        """ Retorna o fecho transitivo de um determinado vértice "vertex" """
+        transitive_clousure.add(vertex)
+
+        for vertexes in self.get_adjacents(vertex):
+            if vertexes not in transitive_clousure:
+                transitive_clousure = self.get_transitive_closure(vertexes, transitive_clousure)
+
+        return transitive_clousure
+    
     def is_connected(self):
+        """ Verifica se o grafo é conexo """
+        set_aux = self.get_transitive_closure(self.get_random_vertex())
+        if set_aux == set(self.dict.keys())
+            return True
+        return False
+
     def is_tree(self):
+        """ Verifica se o grafo é uma árvore """
+        v = self.get_random_vertex()
+        return self.is_connected() and not(self.detect_cycle(v, v))
+
+    def detect_cycle(v, v_before, visited=set()):
+        """ Verifica se o vértice "v" faz parte de algum ciclo """
+        if v in visited:
+            return True
+        visited.add(v)
+        for v_adj in self.get_adjacents(v):
+            if v_adj != v_before:
+                if detect_cycle(v_adj, v, visited):
+                    return True
+        visited.discard(v)
+        return False
